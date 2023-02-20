75e5dc5df8adf64431db042b6440bb73c17ba6d7
Ananias Barbosa <ananiasgomes27@gmail.com>
2023-02-19 21:07:58 -0300

    modificado arquivos de dependencias

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
-import express from "express";
-const app = express();
+import createHttpError from "http-errors"
+import express, { Request, Response, NextFunction, Errback } from "express"
+import path from "path"
+const app = express()
 
+
+import { router as routerCurriculo } from "./routes/curriculo"
+import { router as routerHome } from "./routes/home"
+
+app.use(express.static(path.join(__dirname, "../public")))
 app.use(express.urlencoded({ extended: true }))
-app.use()
+app.use(express.json())
 
-export { app } 
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "../views"))
+
+app.get("/", routerHome)
+app.get("/curriculo", routerCurriculo)
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createHttpError(404))
+})
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('404', { title: "", version: "" });
+})
+
+export { app }
--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ const port = process.env.PORT || "3000"
 const server = app
 
 
-server.listen(port)
+server.listen(port, () => console.log(`SERVIDOR EM EXECUÇÃO NA PORTA: ${port}`))
